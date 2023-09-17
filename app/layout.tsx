cc082bda5dd7a8b6d66c749d4873e586a4f1c128
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2023-09-17 17:33:55 +0700

    Update and Delete the message

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
+        <body
+          className={cn(font.className, 'bg-white dark:bg-[#313338] h-screen')}
+        >
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
